92d3c51c497494c63649eacca0652abff5d97010
lianghua1987 <hua_liang@mckinsey.com>
2023-10-22 16:09:25 +0800

    Add client component

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -1,9 +1,12 @@
 import Heading from "@/components/Heading";
-import {getReview, getReviews, getSlugs} from "@/lib/reviews";
+import {getReview, getSlugs} from "@/lib/reviews";
+import SharedButtons from "@/components/ShareButtons";
 
 export async function generateStaticParams() {
     const slugs = await getSlugs();
-    return slugs.map(slug => {slug});
+    return slugs.map(slug => {
+        slug
+    });
 }
 
 export async function generateMetadata({params: {slug}}) {
@@ -18,7 +21,11 @@ export default async function ReviewPage({params: {slug}}) {
     return (
         <>
             <Heading>{review.title}</Heading>
-            <p className="italic pb-2">{review.date}</p>
+            <div className="flex gap-3 items-baseline">
+                <p className="italic pb-2">{review.date}</p>
+                <SharedButtons/>
+            </div>
+
             <img src={review.imageUrl} alt="" width="640" height="360" className="mb-2 rounded"/>
             <article dangerouslySetInnerHTML={{__html: review.html}}
                      className="prose prose-slate max-w-screen-sm"/>
---
+++ components/ShareButtons.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ShareLinkButton from "@/components/ShareLinkButton";
+
+export default function SharedButtons() {
+    return (
+      <div>
+          <ShareLinkButton/>
+      </div>
+    );
+}
